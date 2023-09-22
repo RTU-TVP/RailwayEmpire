597fa2e0cdd1c6ac548ef54fea9d2b3b0042450c
PuddleWarlock <127037503+PuddleWarlock@users.noreply.github.com>
2023-09-22 17:58:28 +0300

    Coal minigame update

--- src/SuperPuper/Assets/Scripts/Coal Minigame/Coal Spawner.cs
+++ src/SuperPuper/Assets/Scripts/Coal Minigame/Coal Spawner.cs
@@ -8,8 +8,11 @@ public class CoalSpawner : MonoBehaviour
 {
     [SerializeField] private Transform _pos1;
     [SerializeField] private Transform _pos2;
+    [SerializeField] private Transform _pos3;
+    [SerializeField] private Transform _pos4;
     [SerializeField] private GameObject _coal;
     private Random rnd = new Random();
+    private Vector3 _spawnPosition;
     private void Start()
     {
         StartCoroutine(wait());
@@ -19,8 +22,23 @@ public class CoalSpawner : MonoBehaviour
     {
         while (true)
         {
-            int _switch = rnd.Next(0, 2);
-            var coalInst = Instantiate(_coal, _switch == 1 ? _pos1.position : _pos2.position, Quaternion.identity);
+            //int _switch = rnd.Next(0, 4);
+            switch (rnd.Next(0, 4))
+            {
+                case 0:
+                    _spawnPosition = _pos1.position;
+                    break;
+                case 1:
+                    _spawnPosition = _pos2.position;
+                    break;
+                case 2:
+                    _spawnPosition = _pos3.position;
+                    break;
+                case 3:
+                    _spawnPosition = _pos4.position;
+                    break;
+            }
+            var coalInst = Instantiate(_coal,_spawnPosition, Quaternion.identity);
             yield return new WaitForSeconds(1);
         }
     }
--- src/SuperPuper/Assets/Scripts/Coal Minigame/PlayerController.cs
+++ src/SuperPuper/Assets/Scripts/Coal Minigame/PlayerController.cs
@@ -1,11 +1,17 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private int _score;
+    [SerializeField] private Transform _playerPosition1;
+    [SerializeField] private Transform _playerPosition2;
+    [SerializeField] private Transform _playerPosition3;
+    [SerializeField] private Transform _playerPosition4;
+    [SerializeField] private TextMeshProUGUI _scoreScreen;
 
     private void Update()
     {
@@ -15,6 +21,7 @@ public class PlayerController : MonoBehaviour
     public void AddScore(int points)
     {
         _score += points;
+        _scoreScreen.text = "Score: " + _score;
     }
 
     void Move()
@@ -22,10 +29,22 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.A))
         {
             transform.eulerAngles = new Vector3(0, 0, 0);
+            transform.position = _playerPosition1.position;
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
             transform.eulerAngles = new Vector3(0, 180, 0);
+            transform.position = _playerPosition2.position;
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            transform.position = _playerPosition3.position;
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+            transform.position = _playerPosition4.position;
         }
     }
 }
