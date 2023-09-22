91bf343d5ba280752f6499ea9fcff5799a0e70c2
ShutovKS <i@shutovks.ru>
2023-09-22 17:55:25 +0300

    Remove lucky carriage chance

--- src/SuperPuper/Assets/Scripts/Data/Static/Trains/RailwayCarriage.cs
+++ src/SuperPuper/Assets/Scripts/Data/Static/Trains/RailwayCarriage.cs
@@ -6,7 +6,6 @@ namespace Data.Static.Trains
 	[CreateAssetMenu(fileName = "RailwayCarriage", menuName = "Data/Static/Trains/RailwayCarriage")]
 	public class RailwayCarriage : ScriptableObject
 	{
-		[field: Range(0, 1), SerializeField] public float ChanceForMiniGame { get; private set; }
 		[field: SerializeField] public RailwayCarriageType RailwayCarriageType { get; private set; }
 		[field: SerializeField] public GameObject Prefab { get; private set; }
 	}
