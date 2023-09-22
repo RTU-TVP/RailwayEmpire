92455e969eae9f97bfb9a78edb7e9aa9c656ce8a
KotovSergeyV <75029707+KotovSergeyV@users.noreply.github.com>
2023-09-22 17:53:40 +0300

    Numbered Check
    Проверка не по повороту, а по номеру позиции.

--- src/SuperPuper/Assets/Scripts/PipeScript.cs
+++ src/SuperPuper/Assets/Scripts/PipeScript.cs
@@ -8,17 +8,19 @@ public class PipeScript : MonoBehaviour
 {
     private float[] rotations = { 0, 90, 180, 270 };
     [SerializeField] private PipesGameManager _pipesGameManager;
-    [SerializeField] private float[] _correctRotation;
+    [SerializeField] private int[] _correctRotation;
+    [SerializeField] private int _currentRotation;
     [field:SerializeField] public bool IsPlaced { get; private set; } = false;
 
     void Start()
     {
         int rand = Random.Range(0, rotations.Length);
         transform.eulerAngles = new Vector3(0, rotations[rand], 0);
+        _currentRotation = rand;
 
         foreach (float rot in _correctRotation)
         {
-            if (transform.eulerAngles.y == rot)
+            if (_currentRotation == rot)
             {
                 IsPlaced = true;
                 _pipesGameManager.CorrectMove();
@@ -32,10 +34,11 @@ public class PipeScript : MonoBehaviour
     private void OnMouseDown()
     {
         transform.Rotate(new Vector3(0, 90, 0));
-
+        _currentRotation += 1;
+        if (_currentRotation > 3) _currentRotation = 0;
         foreach (float rot in _correctRotation)
         {
-            if (transform.eulerAngles.y == rot)
+            if (_currentRotation == rot)
             {
                 IsPlaced = true;
                 _pipesGameManager.CorrectMove();
