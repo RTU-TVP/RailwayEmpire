14de95aa828c21ac3a03eb04654542c954f2dc6a
PuddleWarlock <127037503+PuddleWarlock@users.noreply.github.com>
2023-09-22 23:54:10 +0300

    Tower Bloxx update

---
+++ src/SuperPuper/Assets/Scripts/Tower Blocks minigame/DefeatCondition.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DefeatCondition : MonoBehaviour {
+
+	private Scene _scene;
+	private MovementSpawn _movementSpawn;
+	
+
+	void Start ()
+	{
+		_movementSpawn = FindObjectOfType<MovementSpawn>();
+		_scene = SceneManager.GetActiveScene ();
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("StopTrigger"))
+		{
+			gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+			_movementSpawn.AddScore(1);
+		}
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.CompareTag("Floor")) {
+			SceneManager.LoadScene (_scene.name);
+		}
+	}
+}
