774dafba858bfbc14ee558851feb575460a5c78b
ShutovKS <i@shutovks.ru>
2023-09-23 00:01:08 +0300

    Created FSM for workers

---
+++ src/SuperPuper/Assets/Scripts/Units/Workers/State Machine/StateMachine.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Workers.State_Machine
+{
+	public class StateMachine
+	{
+		private readonly static List<Transition> EmptyTransitions = new List<Transition>(0);
+
+		private readonly List<Transition> _anyTransitions = new List<Transition>();
+
+		private readonly Dictionary<Type, List<Transition>> _transitions = new Dictionary<Type, List<Transition>>();
+
+		private IState _currentState;
+
+		private List<Transition> _currentTransitions = new List<Transition>();
+
+		public void Tick()
+		{
+			var transition = GetTransition();
+			if (transition != null) SetState(transition.To);
+
+			_currentState?.Tick();
+		}
+
+		public void SetState(IState state)
+		{
+			if (state == _currentState)
+				return;
+
+			_currentState?.OnExit();
+			_currentState = state;
+			_transitions.TryGetValue(_currentState.GetType(), out _currentTransitions);
+			_currentTransitions ??= EmptyTransitions;
+
+			_currentState.OnEnter();
+
+			Debug.Log(_currentState.GetType().Name);
+		}
+
+		public void AddTransition(IState to, IState from, Func<bool> predicate)
+		{
+			if (_transitions.TryGetValue(from.GetType(), out var transitions) == false)
+			{
+				transitions = new List<Transition>();
+				_transitions[from.GetType()] = transitions;
+			}
+
+			transitions.Add(new Transition(to, predicate));
+		}
+
+		public void AddAnyTransition(IState state, Func<bool> predicate)
+		{
+			_anyTransitions.Add(new Transition(state, predicate));
+		}
+
+		private Transition GetTransition()
+		{
+			foreach (var transition in _anyTransitions)
+			{
+				if (transition.Condition())
+					return transition;
+			}
+
+			foreach (var transition in _currentTransitions)
+			{
+				if (transition.Condition())
+					return transition;
+			}
+
+			return null;
+		}
+	}
+}
