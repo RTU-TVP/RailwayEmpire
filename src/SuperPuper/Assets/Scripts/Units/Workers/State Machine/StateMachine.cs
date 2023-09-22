e06cb7590d38297c4754424bfeea08e097a212f0
ShutovKS <i@shutovks.ru>
2023-09-23 01:55:50 +0300

    Added worker manager

--- src/SuperPuper/Assets/Scripts/Units/Workers/State Machine/StateMachine.cs
+++ src/SuperPuper/Assets/Scripts/Units/Workers/State Machine/StateMachine.cs
@@ -1,75 +1,58 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Workers.State_Machine
 {
-	public class StateMachine
-	{
-		private readonly static List<Transition> EmptyTransitions = new List<Transition>(0);
+    public class StateMachine
+    {
+        private readonly static List<Transition> EmptyTransitions = new List<Transition>(0);
 
-		private readonly List<Transition> _anyTransitions = new List<Transition>();
+        private readonly Dictionary<Type, List<Transition>> _transitions = new Dictionary<Type, List<Transition>>();
 
-		private readonly Dictionary<Type, List<Transition>> _transitions = new Dictionary<Type, List<Transition>>();
+        private IState _currentState;
 
-		private IState _currentState;
+        private List<Transition> _currentTransitions = new List<Transition>();
 
-		private List<Transition> _currentTransitions = new List<Transition>();
+        public void Tick()
+        {
+            var transition = GetTransition();
+            if (transition != null) SetState(transition.To);
 
-		public void Tick()
-		{
-			var transition = GetTransition();
-			if (transition != null) SetState(transition.To);
+            _currentState?.Tick();
+        }
 
-			_currentState?.Tick();
-		}
+        public void SetState(IState state)
+        {
+            if (state == _currentState)
+                return;
 
-		public void SetState(IState state)
-		{
-			if (state == _currentState)
-				return;
+            _currentState?.OnExit();
+            _currentState = state;
+            _transitions.TryGetValue(_currentState.GetType(), out _currentTransitions);
+            _currentTransitions ??= EmptyTransitions;
 
-			_currentState?.OnExit();
-			_currentState = state;
-			_transitions.TryGetValue(_currentState.GetType(), out _currentTransitions);
-			_currentTransitions ??= EmptyTransitions;
+            _currentState.OnEnter();
 
-			_currentState.OnEnter();
+            Debug.Log(_currentState.GetType().Name);
+        }
 
-			Debug.Log(_currentState.GetType().Name);
-		}
+        public void AddTransition(IState to, IState from, Func<bool> predicate)
+        {
+            if (_transitions.TryGetValue(from.GetType(), out var transitions) == false)
+            {
+                transitions = new List<Transition>();
+                _transitions[from.GetType()] = transitions;
+            }
 
-		public void AddTransition(IState to, IState from, Func<bool> predicate)
-		{
-			if (_transitions.TryGetValue(from.GetType(), out var transitions) == false)
-			{
-				transitions = new List<Transition>();
-				_transitions[from.GetType()] = transitions;
-			}
+            transitions.Add(new Transition(to, predicate));
+            Debug.Log(transitions.Count);
+        }
 
-			transitions.Add(new Transition(to, predicate));
-		}
-
-		public void AddAnyTransition(IState state, Func<bool> predicate)
-		{
-			_anyTransitions.Add(new Transition(state, predicate));
-		}
-
-		private Transition GetTransition()
-		{
-			foreach (var transition in _anyTransitions)
-			{
-				if (transition.Condition())
-					return transition;
-			}
-
-			foreach (var transition in _currentTransitions)
-			{
-				if (transition.Condition())
-					return transition;
-			}
-
-			return null;
-		}
-	}
+        private Transition GetTransition()
+        {
+            return _currentTransitions.FirstOrDefault(transition => transition.Condition());
+        }
+    }
 }
---
+++ src/SuperPuper/Assets/Scripts/Units/Workers/WorkersManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Workers
+{
+    public class WorkersManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject _workerPrefab;
+        [SerializeField] private Transform _spawnPoint;
+        [SerializeField] private Transform _home;
+
+        private void CreateWorker(Transform target)
+        {
+            var worker = Instantiate(_workerPrefab, _spawnPoint.position, Quaternion.identity);
+            worker.GetComponent<Worker>().SetUp(target, _home);
+        }
+    }
+}
