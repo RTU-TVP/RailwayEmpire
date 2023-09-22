d6ec1ef61de134a9b4a3d5678429ad2f007076b2
ShutovKS <i@shutovks.ru>
2023-09-23 01:01:14 +0300

    Added works configuration

---
+++ src/SuperPuper/Assets/Scripts/Data/Static/Workers/WorkersConfiguration.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Data.Static.Workers
+{
+    [CreateAssetMenu(fileName = "WorkersConfiguration", menuName = "Data/Static/Workers/WorkersConfiguration")]
+    public class WorkersConfiguration : ScriptableObject
+    {
+        public float WorkTimeDefault;
+    }
+}
