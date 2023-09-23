0fc1c27fe0c2fbeb0f409a4621abd5aca9802a3c
OlegMorev <proks200413@gmail.com>
2023-09-23 03:32:06 +0300

    UI cosmetics

--- src/SuperPuper/Assets/Scripts/MoneyStats.cs
+++ src/SuperPuper/Assets/Scripts/MoneyStats.cs
@@ -1,6 +1,6 @@
 public static class MoneyAndUpgradesStats
 {
-    public static int money;
+    public static int money=500;
     public static int levelWalkSpeed;
     public static int levelUnloadSpeed;
     public static int levelSellSpeed;
