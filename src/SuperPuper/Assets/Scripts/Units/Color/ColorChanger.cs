aa5207f7801ca553d304e33de0f61319e3180009
ShutovKS <i@shutovks.ru>
2023-09-23 11:30:04 +0300

    comit

--- src/SuperPuper/Assets/Scripts/Units/Color/ColorChanger.cs
+++ src/SuperPuper/Assets/Scripts/Units/Color/ColorChanger.cs
@@ -6,12 +6,12 @@ using UnityEngine;
 
 public class ColorChanger : MonoBehaviour
 {
-    [field: SerializeField] private IdToMaterial[] _cells;
+    [field: SerializeField] private MaterialData[] _cells;
 
     private void Start()
     {
         MeshRenderer renderer = gameObject.GetComponent<MeshRenderer>();
-        foreach (IdToMaterial cell in _cells)
+        foreach (MaterialData cell in _cells)
         {
             renderer.materials[cell.MatIndex].color = cell.Colors[Random.Range(0, cell.Colors.Length)];
         }
--- src/SuperPuper/Assets/Scripts/Units/Color/MaterialData.cs
+++ src/SuperPuper/Assets/Scripts/Units/Color/MaterialData.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 #endregion
 
 [Serializable]
-public class IdToMaterial
+public class MaterialData
 {
     [field: SerializeField] public int MatIndex { get; private set; }
     [field: SerializeField] public Color[] Colors { get; private set; }
