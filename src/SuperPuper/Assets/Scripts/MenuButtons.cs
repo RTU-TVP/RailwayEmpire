0558de953c806045b1b9f6b253011ea606da624e
OlegMorev <proks200413@gmail.com>
2023-09-23 12:12:33 +0300

    continue added to menu

--- src/SuperPuper/Assets/Scripts/MenuButtons.cs
+++ src/SuperPuper/Assets/Scripts/MenuButtons.cs
@@ -8,9 +8,8 @@ using UnityEngine.UIElements;
 public class MenuButtons : MonoBehaviour
 {
     [SerializeField] GameObject _progressBar;
-    public void StartLevel()
+    public void NewGame()
     {
-
         _progressBar.SetActive(true);
         StartCoroutine(GameSceneStarter("GameTestScene"));
         gameObject.SetActive(false);
@@ -30,5 +29,9 @@ public class MenuButtons : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+    public void ContinueGame()
+    {
+        Debug.Log("Continue");
     }
 }
