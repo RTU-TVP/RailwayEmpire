0d83eba0783f6d43c7df988e75239e886ddc7976
ShutovKS <i@shutovks.ru>
2023-09-23 12:23:31 +0300

    Added stop poin in rail track

--- src/SuperPuper/Assets/Scripts/Units/Train/RailTrack.cs
+++ src/SuperPuper/Assets/Scripts/Units/Train/RailTrack.cs
@@ -9,6 +9,7 @@ namespace Train
     public class RailTrack : MonoBehaviour
     {
         [field: SerializeField] public Transform StartPoint { get; private set; }
+        [field: SerializeField] public Transform StopPoint { get; private set; }
         [field: SerializeField] public Transform EndPoint { get; private set; }
         public bool IsOccupied { get; private set; }
 
