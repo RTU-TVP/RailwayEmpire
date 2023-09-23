a5e07cc44408021ca7be444179a10eb6b5404f68
PuddleWarlock <127037503+PuddleWarlock@users.noreply.github.com>
2023-09-23 22:00:20 +0300

    Bloxx fix + box replace

--- src/SuperPuper/Assets/Scripts/Units/Minigames/TowerBlocks/MovementSpawn.cs
+++ src/SuperPuper/Assets/Scripts/Units/Minigames/TowerBlocks/MovementSpawn.cs
@@ -1,57 +1,85 @@
-﻿#region
-
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-#endregion
+public class MovementSpawn : MonoBehaviour {
 
-namespace Units.Minigames.TowerBlocks
-{
-    public class MovementSpawn : MonoBehaviour
-    {
-        public UnityEngine.Camera camaraPrincipal;
-        [SerializeField] private TextMeshProUGUI _scoreText;
-        private int _score;
+	private bool conditionMovementHorizontal = true;
+	public Camera cameraPrincipal;
+	private int _score;
+	public GameObject CubePrefab;
+	private bool _canSpawn = true;
+	private float _spawnTimer = 0;
+	public bool _hasStarted;
+	[SerializeField] private TextMeshProUGUI _scoreText;
 
-        private bool conditionMovementHorizontal = true;
-        private bool conditionMovementVertical = true;
+	private void Start()
+	{
+		cameraPrincipal = FindObjectOfType<Camera>();
+	}
 
-        private void Update()
-        {
+	void Update () 
+	{
+		if (_hasStarted)
+		{
+			Movement();
+			UpdateTime();
+		}
+	}
 
-            if (Input.GetKeyDown(KeyCode.Space) && conditionMovementVertical)
-            {
-                transform.Translate(new Vector3(0, 1.9f, 0));
-                camaraPrincipal.transform.Translate(new Vector3(0, 1.9f, 0));
-                conditionMovementVertical = false;
-            }
-            if (transform.position.x <= 6 && conditionMovementHorizontal)
-            {
-                transform.Translate(Vector3.right * (Time.deltaTime * 5));
-                if (transform.position.x >= 6)
-                {
-                    conditionMovementHorizontal = false;
-                }
-            }
-            if (!conditionMovementHorizontal)
-            {
-                transform.Translate(-Vector3.right * (Time.deltaTime * 5));
-                if (transform.position.x <= -6)
-                {
-                    conditionMovementHorizontal = true;
-                }
-            }
-            conditionMovementVertical = true;
-        }
+	private void Movement()
+	{
+		if (Input.GetKeyDown (KeyCode.Space)  && _canSpawn) 
+		{
+			SpawnCube();
+			transform.Translate (new Vector3(0,1.9f,0));
+			cameraPrincipal.transform.Translate (new Vector3(0,1.9f,0));
+		}
+		if (transform.position.x <= 6 && conditionMovementHorizontal) 
+		{
+			transform.Translate (Vector3.right * (Time.deltaTime * 5));
+			if (transform.position.x >= 6) 
+			{
+				conditionMovementHorizontal = false;
+			}
+		}
+		if (!conditionMovementHorizontal) 
+		{
+			transform.Translate (-Vector3.right * (Time.deltaTime * 5));
+			if (transform.position.x <= -6) 
+			{
+				conditionMovementHorizontal = true;
+			}
+		}
+	}
 
-        public void AddScore(int points)
-        {
-            _score += points;
-            _scoreText.text = "Score: " + _score;
-            if (_score == 10)
-            {
-                print("Winner Winner Chicken Dinner");
-            }
-        }
-    }
+	public void AddScore(int points)
+	{
+		_score += points;
+		_scoreText.text = "Score: " + _score;
+		if (_score == 10)
+		{
+			print("Winner Winner Chicken Dinner");
+		}
+	}
+
+	private void UpdateTime()
+	{
+		_spawnTimer += Time.deltaTime;
+		if (_spawnTimer >= 1.0f)
+		{
+			_spawnTimer = 0;
+			_canSpawn = true;
+		}
+	}
+
+	private void SpawnCube()
+	{
+		if (Input.GetKeyDown (KeyCode.Space) && _canSpawn) {
+			Instantiate (CubePrefab, transform.position, new Quaternion(-90,0,90,0));
+			_canSpawn = false;
+		}
+	}
 }
