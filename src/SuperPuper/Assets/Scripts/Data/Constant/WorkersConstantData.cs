eda274316bb650ef4153a8fb446a939739fe41ae
ShutovKS <i@shutovks.ru>
2023-09-23 09:14:13 +0300

    Added workers constant data

---
+++ src/SuperPuper/Assets/Scripts/Data/Constant/WorkersConstantData.cs
@@ -0,0 +1,9 @@
+namespace Data.Constant
+{
+    public static class WorkersConstantData
+    {
+        public const string WORKERS_LVL_MOVE_SPEED = "WorkersLvlMoveSpeed";
+        public const string WORKERS_LVL_WORK_TIME = "WorkersLvlWorkTime";
+        public const string WORKERS_LVL_SALE_TIME = "WorkersLvlSaleTime";
+    }
+}
