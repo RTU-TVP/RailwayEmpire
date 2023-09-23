3d6ad776852b93802e8d2b664114565eac3f7ef0
ShutovKS <i@shutovks.ru>
2023-09-23 13:49:42 +0300

    Update RailwayCarriagesDatabase

--- src/SuperPuper/Assets/Scripts/Data/Static/Trains/RailwayCarriagesDatabase.cs
+++ src/SuperPuper/Assets/Scripts/Data/Static/Trains/RailwayCarriagesDatabase.cs
@@ -1,6 +1,9 @@
 #region
 
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 #endregion
 
@@ -9,12 +12,31 @@ namespace Data.Static.Trains
     [CreateAssetMenu(fileName = "RailwayCarriagesDataBase", menuName = "Data/Static/Trains/RailwayCarriagesDataBase")]
     public class RailwayCarriagesDatabase : ScriptableObject
     {
-        [field: SerializeField] public RailwayCarriage[] RailwayCarriages { get; private set; }
+        [field: SerializeField] private RailwayCarriage[] _railwayCarriages;
+        private Dictionary<RailwayCarriageType, RailwayCarriage> _railwayCarriagesDictionary;
+
+        private void OnEnable()
+        {
+            _railwayCarriagesDictionary = new Dictionary<RailwayCarriageType, RailwayCarriage>();
+            foreach (var railwayCarriage in _railwayCarriages)
+            {
+                _railwayCarriagesDictionary.Add(railwayCarriage.RailwayCarriageType, railwayCarriage);
+            }
+        }
 
         public RailwayCarriage GetRandomRailwayCarriage()
         {
-            int randomIndex = Random.Range(0, RailwayCarriages.Length);
-            return RailwayCarriages[randomIndex];
+            int randomIndex = Random.Range(0, _railwayCarriages.Length);
+            return _railwayCarriages[randomIndex];
+        }
+
+        public RailwayCarriage GetRailwayCarriage(RailwayCarriageType railwayCarriageType)
+        {
+            if (_railwayCarriagesDictionary.ContainsKey(railwayCarriageType))
+            {
+                return _railwayCarriagesDictionary[railwayCarriageType];
+            }
+            throw new Exception($"RailwayCarriage with type {railwayCarriageType} not found");
         }
     }
 }
