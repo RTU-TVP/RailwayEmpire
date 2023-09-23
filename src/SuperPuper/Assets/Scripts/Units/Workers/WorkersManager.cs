eb3c8106f8eaf5e08a0ae8db955c28c17985dc51
ShutovKS <i@shutovks.ru>
2023-09-23 09:00:54 +0300

    Added workers upgrade

--- src/SuperPuper/Assets/Scripts/Units/Workers/WorkersManager.cs
+++ src/SuperPuper/Assets/Scripts/Units/Workers/WorkersManager.cs
@@ -1,17 +1,30 @@
+using Data.Constant;
+using Data.Static.Workers;
 using UnityEngine;
 
 namespace Workers
 {
     public class WorkersManager : MonoBehaviour
     {
+        [SerializeField] private WorkersConfiguration _workersConfiguration;
         [SerializeField] private GameObject _workerPrefab;
         [SerializeField] private Transform _spawnPoint;
         [SerializeField] private Transform _home;
 
         private void CreateWorker(Transform target)
         {
+            var moveSpeedLvl = PlayerPrefs.GetInt(WorkersConstantData.WORKERS_LVL_MOVE_SPEED);
+            var workTimeLvl = PlayerPrefs.GetInt(WorkersConstantData.WORKERS_LVL_WORK_TIME);
+            var saleTimeLvl = PlayerPrefs.GetInt(WorkersConstantData.WORKERS_LVL_SALE_TIME);
+
             var worker = Instantiate(_workerPrefab, _spawnPoint.position, Quaternion.identity);
-            worker.GetComponent<Worker>().SetUp(target, _home, () => Debug.Log("Work done!"));
+            worker.GetComponent<Worker>().SetUp(
+                target,
+                _home,
+                () => Debug.Log("Work done!"),
+                _workersConfiguration.MoveSpeedDefault + moveSpeedLvl * _workersConfiguration.MoveSpeedDefault * 0.01f,
+                _workersConfiguration.WorkTimeDefault - workTimeLvl * _workersConfiguration.WorkTimeDefault * 0.01f,
+                _workersConfiguration.SaleTimeDefault - saleTimeLvl * _workersConfiguration.SaleTimeDefault * 0.01f);
         }
     }
 }
