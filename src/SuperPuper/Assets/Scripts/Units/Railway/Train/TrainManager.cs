6da3ee9c5e7e325c0de7bfdfd4a6c638bb6c01f3
KotovSergeyV <75029707+KotovSergeyV@users.noreply.github.com>
2023-09-24 07:44:29 +0300

    PipesMaksimFIX

--- src/SuperPuper/Assets/Scripts/Units/Railway/Train/TrainManager.cs
+++ src/SuperPuper/Assets/Scripts/Units/Railway/Train/TrainManager.cs
@@ -1,16 +1,16 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace Units.Train
-{
-    public class TrainManager
-    {
-        public TrainManager(RailwayCarriageManager[] railwayCarriageManagers)
-        {
-            _railwayCarriageManagers = railwayCarriageManagers;
-        }
-
-        private Dictionary<Type
-        private int 
-    }
-}
+// using System.Collections.Generic;
+// using UnityEngine;
+//
+// namespace Units.Train
+// {
+//     public class TrainManager
+//     {
+//         public TrainManager(RailwayCarriageManager[] railwayCarriageManagers)
+//         {
+//             _railwayCarriageManagers = railwayCarriageManagers;
+//         }
+//
+//         private Dictionary<Type
+//         private int 
+//     }
+// }
