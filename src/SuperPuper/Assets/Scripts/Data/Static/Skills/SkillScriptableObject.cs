e76027ba8550422cb85c4c33cbf0b5ba755b51fe
ShutovKS <schutow.kir@yandex.ru>
2023-09-25 11:00:22 +0300

    Added data static for skill

---
+++ src/SuperPuper/Assets/Scripts/Data/Static/Skills/SkillScriptableObject.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Data.Static.Skills
+{
+    [CreateAssetMenu(fileName = "Skill", menuName = "Data/Static/Skills/Skill", order = 0)]
+    public class SkillScriptableObject : ScriptableObject
+    {
+        [field: SerializeField] public int AdditionalPrice { get; private set; }
+        [field: SerializeField] public string Description { get; private set; }
+        [field: SerializeField] public Sprite Icon { get; private set; }
+    }
+}
